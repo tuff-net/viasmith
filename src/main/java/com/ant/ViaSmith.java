e2ae476481db0d7e66de6c1808406b185f2a346b
antgame11 <68531196+antgame11@users.noreply.github.com>
2025-10-21 15:57:48 +0000

    remove mixin and remove other placeholders

--- src/main/java/com/ant/ViaSmith.java
+++ src/main/java/com/ant/ViaSmith.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class ViaSmith implements ModInitializer {
 	public static final String MOD_ID = "viasmith";
 
 	// This logger is used to write text to the console and the log file.
